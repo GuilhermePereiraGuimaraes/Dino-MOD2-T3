879f22b07b80eec222e1d224e11c642d20b5d2e5
GuilhermePereiraGuimaraes <guilhermezemo@gmail.com>
2022-10-26 22:54:24 -0300

    z

--- dino_runner/game.py
+++ dino_runner/game.py
@@ -2,7 +2,7 @@
 import pygame
 from dino_runner.utils.constants import BG,ICON,SCREEN_HEIGHT,SCREEN_WIDTH,TITLE,FPS,DEFAULT_TYPE
 from dino_runner.components.dinosaur import Dinosaur
-from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.obstacles.obstacles_manager import ObstacleManager
 from dino_runner.utils.text_utils import draw_message_component
 from dino_runner.components.powerups.power_up_manager import PowerUpManager
 
